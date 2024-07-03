031a9b2f559ae52e416f1274dae5a5eefa38f9f1
Amy-S-Anderson <anderson.amy.susan@gmail.com>
2024-07-03 16:22:20 +0200

    Created separate file to hold plotting functions for model output.

---
+++ Functions/Plotting_Functions.R
@@ -0,0 +1,17 @@
+
+
+
+
+############ PLOT MODEL OUTPUT ##############
+
+
+#### Check population size over time
+pop_size <- function(output){
+  pop_size_table <- output %>%
+  group_by(year) %>%
+  summarise(pop_count = n())
+pop_size_table
+
+ggplot(pop_size_table, aes(x = year, y = pop_count)) +
+  geom_line()
+}
