9f7a52b54455c14fd238b811c6d7a489883c565f
Amy-S-Anderson <anderson.amy.susan@gmail.com>
2024-07-03 16:07:47 +0200

    Created a 'Make' file to load packages and function files in appropriate order.

---
+++ Make_File.R
@@ -0,0 +1,31 @@
+
+
+
+###################### MAKE FILE ############################
+
+# Run this first!
+
+# It loads all the necessary libraries, and loads the function files for the model in the correct order so that functions that depend on objects or functions defined in other files are loaded without errors. 
+
+
+### Load libraries
+library(tidyverse)
+library(cowplot)
+
+
+
+
+### Load bespoke functions
+# Note: # Run the source() command separately for each function file so that, if something throws an error, the console will give you an error message that tells you exactly which file is glitching when it tries to load. 
+source("./Functions//Generate Time0 Agents.R")
+source("./Functions//Generate Agent IDs.R")
+source("./Functions//Choose languages.R")
+source("./Functions//Marriage.R")
+source("./Functions//Calculate Agent Births.R")
+source("./Functions//Calculate Agent Deaths.R")
+source("./Functions//Pair agents for conversations.R")
+source("./Functions//Learn by Language Immersion.R")
+source("./Functions//Plotting_Functions.R")
+
+
+
