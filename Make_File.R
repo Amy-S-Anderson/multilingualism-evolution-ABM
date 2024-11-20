5c5a4f9958b530af25e148a0cf43e30603cd485b
Amy-S-Anderson <anderson.amy.susan@gmail.com>
2024-11-20 17:02:13 +0100

    Make file now calls a single file that contains all the functions in the Language Choice ABM, and the script for the ABM itself.

--- Make_File.R
+++ Make_File.R
@@ -20,14 +20,8 @@ library(data.table)
 ### Load bespoke functions
 # Note: This is one time that you don't want to aim for efficiency by using an apply function rather than repeating code.
 # Run the source() command separately for each function file so that, if something throws an error, the console will give you an error message that tells you exactly which file is glitching when it tries to load. 
-source("./Functions//Generate Time0 Agents.R")
-source("./Functions//Generate Agent IDs.R")
-source("./Functions//Choose languages.R")
-source("./Functions//Marriage.R")
-source("./Functions//Calculate Agent Births.R")
-source("./Functions//Calculate Agent Deaths.R")
-source("./Functions//Pair agents for conversations.R")
-source("./Functions//Learn by Language Immersion.R")
+source("./Functions//All Functions for Parental Language Choice Model.R")
+source("./Parental Language Choice Model Function.R")
 source("./Functions//Plotting_Functions.R")
 
 
