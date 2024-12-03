f56775a113ae83abba80da62798bafdafc1c853d
Amy-S-Anderson <anderson.amy.susan@gmail.com>
2024-12-03 15:03:22 +0100

    Make file now calls files for the newest model version: 'How to survive a killer language'

--- Make_File.R
+++ Make_File.R
@@ -20,8 +20,8 @@ library(data.table)
 ### Load bespoke functions
 # Note: This is one time that you don't want to aim for efficiency by using an apply function rather than repeating code.
 # Run the source() command separately for each function file so that, if something throws an error, the console will give you an error message that tells you exactly which file is glitching when it tries to load. 
-source("./Functions//All Functions for Parental Language Choice Model.R")
-source("./Parental Language Choice Model Function.R")
+source("./Functions//All Functions - How to Survive a Killer Language.R")
+#source("./Parental Language Choice Model Function.R")
 source("./Functions//Plotting_Functions.R")
 
 
